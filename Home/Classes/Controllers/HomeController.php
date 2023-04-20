efcd29f2ec31e99846b18215b405d5264c0d8c42
Ghostick <paladiy.daniil@gmail.com>
2023-04-20 18:09:06 +0300

    hometask+ 19.04.2023

--- Home/Classes/Controllers/HomeController.php
+++ Home/Classes/Controllers/HomeController.php
@@ -16,8 +16,19 @@ class HomeController implements ControllerMethodName
 
     public function index() : void{
 
-        // Просто передали назву сторінки в шаблон
+        $home = [
+            [
+                'title' => 'Это домашняя страница. Чувствуй себя как дома.',
+                'text'  => 'Тут ты можеш узнать основные существующие страницы на данный момент',
+                'pages0'=> 'Есть раздел новостей, где всегда честные и свежие новости: /news',
+                'pages1'=> 'Что бы всегда вернуться сюда используй: /home',
+                'pages2'=> 'У нас есть форум, обязательно навести его: /forum',
+                'pages3'=>'Так же у тебя есть профиль: /profile'
+            ]
 
-        new Rendering(['page' => 'home']);
+        ];
+
+        $data = ['home' => $home, 'page' => 'home'];
+        new Rendering($data);
     }
 }
